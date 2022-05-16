02045c765a4b627bccd024fb4042af6a5378fd9f
Roy Sanderson <roy.sanderson@ncl.ac.uk>
2022-05-16 14:39:11 +0100

    Simplified server logic by adding a function display_vector to deal with display and download

--- R/vector_plot.R
+++ R/vector_plot.R
@@ -19,3 +19,11 @@ wader_areas_ll <- st_transform(wader_areas_os, 4326)
 
 wader_region_os <- readRDS("data/wader_region.RDS")
 wader_region_ll <- st_transform(wader_region_os, 4326)
+
+
+
+vector_df <- data.frame(
+  selection = c("Whole area", "IHU areas", "Wader groups", "Wader areas", "Wader region"),
+  ll_map    = c("coast_ll", "areas_ll", "wader_groups_ll", "wader_areas_ll", "wader_region_ll"),
+  os_map    = c("coast_os", "areas_os", "wader_groups_os", "wader_areas_os", "wader_region_os")
+)
--- app.R
+++ app.R
@@ -13,9 +13,9 @@ ui <- fluidPage(
     navlistPanel(
         id = "tabset",
         "Wader DB",
-        tabPanel("Vector",
-                 h2("Vector data"),
-                 p("Select catchment and river data from this page"),
+        tabPanel("Main regions",
+                 h2("Regional boundaries"),
+                 p("Select catchment Integrated Hydrological Units (EA) from this page"),
                  radioButtons("region", "Select vector dataset", vector_regions),
                  leafletOutput("vector_map"),
                  p(),
@@ -27,108 +27,35 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
+    
+    display_vector <- function(selected_region, vector_df){
+        this_choice <- filter(vector_df, selected_region == selection)
+            output$vector_map <- renderLeaflet({
+                leaflet() %>%
+                    addTiles() %>%
+                    addProviderTiles(providers$Esri.WorldImagery,
+                                     options = providerTileOptions(noWrap = TRUE)
+                    ) %>%
+                    addFeatures(get(this_choice[1,"ll_map"]))
+            })
+            output$download_vect <- downloadHandler(
+                filename = function() {
+                    paste0(input$download_vect, ".RDS")
+                },
+                content = function(file) {
+                    saveRDS(get(this_choice[1, "os_map"]), file)
+                })
+    }
     
     observeEvent(input$region, {
         selected_region <- input$region
         #cat(file=stderr(), "selected region is", input$region, "\n")
-        if(selected_region == "Whole area"){
-            output$vector_map <- renderLeaflet({
-                leaflet() %>%
-                    addTiles() %>% 
-                    addProviderTiles(providers$Esri.WorldImagery,
-                                     options = providerTileOptions(noWrap = TRUE)
-                    ) %>%
-                    addFeatures(coast_ll) 
-            })
-            output$download_vect <- downloadHandler(
-                filename = function() {
-                    paste0(input$dataset, ".RDS")
-                },
-                content = function(file) {
-                    saveRDS(coast_os, file)
-                })
-
-           
-        } else if(selected_region == "IHU areas"){
-            output$vector_map <- renderLeaflet({
-                leaflet() %>%
-                    addTiles() %>% 
-                    addProviderTiles(providers$Esri.WorldImagery,
-                                     options = providerTileOptions(noWrap = TRUE)
-                    ) %>%
-                    addFeatures(areas_ll)
-            })
-            output$download_vect <- downloadHandler(
-                filename = function() {
-                    paste0(input$dataset, ".RDS")
-                },
-                content = function(file) {
-                    saveRDS(areas_os, file)
-                })
-            
-        } else if(selected_region == "Wader groups"){
-            output$vector_map <- renderLeaflet({
-                leaflet() %>%
-                    addTiles() %>% 
-                    addProviderTiles(providers$Esri.WorldImagery,
-                                     options = providerTileOptions(noWrap = TRUE)
-                    ) %>%
-                    addFeatures(wader_groups_ll)
-            })
-            output$download_vect <- downloadHandler(
-                filename = function() {
-                    paste0(input$dataset, ".RDS")
-                },
-                content = function(file) {
-                    saveRDS(wader_groups_os, file)
-                })
-            
-        } else if(selected_region == "Wader areas"){
-            output$vector_map <- renderLeaflet({
-                leaflet() %>%
-                    addTiles() %>% 
-                    addProviderTiles(providers$Esri.WorldImagery,
-                                     options = providerTileOptions(noWrap = TRUE)
-                    ) %>%
-                    addFeatures(wader_areas_ll)
-            })
-            output$download_vect <- downloadHandler(
-                filename = function() {
-                    paste0(input$dataset, ".RDS")
-                },
-                content = function(file) {
-                    saveRDS(wader_areas_os, file)
-                })
-         } else if(selected_region == "Wader region"){
-                output$vector_map <- renderLeaflet({
-                    leaflet() %>%
-                        addTiles() %>% 
-                        addProviderTiles(providers$Esri.WorldImagery,
-                                         options = providerTileOptions(noWrap = TRUE)
-                        ) %>%
-                        addFeatures(wader_region_ll)
-                })
-                output$download_vect <- downloadHandler(
-                    filename = function() {
-                        paste0(input$dataset, ".RDS")
-                    },
-                    content = function(file) {
-                        saveRDS(wader_region_os, file)
-                    })
-                
- 
-        }
+        
+        display_vector(selected_region, vector_df)
     })
 
-    # output$download_vect <- downloadHandler(
-    #     filename = function() {
-    #         paste0(input$dataset, ".RDS")
-    #     },
-    #     content = function(file) {
-    #         saveRDS(data_out, file)
-    #     })
 }
 
 # Run the application 
